889c5bc06dfe95b27a3e90127f81ef78c498b676
surwiwdok17-boop <surwiwdok17@ukr.net>
2025-12-04 19:27:21 +0200

    Delete Лабораторна 5/Лаборатока 4) directory

--- Лабораторна 5/Лаборатока 4)/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from .shop import shop_bp
-from .feedback import feedback_bp
-from .admin import init_admin
-
-blueprints = [shop_bp, feedback_bp]
-from .shop import shop_bp
-from .feedback import feedback_bp
-from .admin import init_admin
-
